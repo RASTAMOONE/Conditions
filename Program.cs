f5834455faf341d9f015fe51716ddd3008d74c85
RASTAMOONE <108339727+RASTAMOONE@users.noreply.github.com>
2022-10-09 22:39:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите х:");
+string x = Console.ReadLine();
+Console.Write("Введите у:");
+string y = Console.ReadLine();
+int xx = Convert.ToInt32(x);
+int yy = Convert.ToInt32(y);
+int sum = xx + yy;
+if (xx == yy)
+{
+    Console.WriteLine($"Сумма равняется {xx}");
+}
+else
+{
+    Console.WriteLine($"Сумма равняется{sum}");
+}
